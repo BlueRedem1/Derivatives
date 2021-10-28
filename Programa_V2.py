021dca362727bb1de2a2f51407c9386e7ac42dfe
Arath Alejandro Reyes Lopez <63680407+ArathReyes@users.noreply.github.com>
2021-10-28 10:26:53 -0500

    Create Programa_V2.py

---
+++ Programa_V2.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 28 10:03:14 2021
+
+@author: Arath Reyes
+"""
+import pandas as pd
+import numpy as np
+
+def VanillaEuropenOption():
+    
+    return
+
+def VanillaAmericanOption():
+    
+    return
+
+def AsianOption():
+    
+    return
+
+def Forward():
+    
+    return
+
+def UpNInIOption():
+    
+    return
+
+def UpNOutOption():
+    
+    return
+
+def LookBackOption():
+    
+    return
+
+def DigitalOption():
+    
+    return
+
+
+class Derivative():
+    
+    def __init__(self):
+        self.strike = 0.0
+        self.payoff = 0.0
+        self.volatility = 0.0
+        self.rate = 0.0
+        self.periods = 0.0
+        self.date = 0.0
+        self.delta = []
+        self.S0 = 0.0
+        self.q = 0.0
+        self.price = 0.0
+        
+    def compute(self, kind = "European", Call = True, *args):
+        
+        return
+
+    def plot(self):
+        
+        return
+    
+    def summary(self):
+        
+        return
+
